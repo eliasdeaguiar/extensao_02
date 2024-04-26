7339d5e280631754ba42b3c1f170b292473bf85e
Elias Aguiar <elias.aguiar@hotmail.com>
2024-04-26 18:19:43 -0300

    Adicionando arquivos da pasta para o repositório extensao_02

---
+++ orcamentos.js
@@ -0,0 +1,54 @@
+const orcamentos = [
+    {
+        orcamento: 0,
+        paciente: 'Paulo Machado',
+        situacao: 'Em espera',
+        celular: '(34) 99246-9033',
+        whatsapp: '(34) 99246-9033',
+        dataorcamento: '02/02/2024',
+        convenio: 'UNIMED'
+    },
+    {
+        orcamento: 1,
+        paciente: 'Mariana Gomes',
+        situacao: 'Em espera',
+        celular: '(34) 99246-9033',
+        whatsapp: '(34) 99246-9033',
+        dataorcamento: '12/02/2024',
+        convenio: 'UNIMED'
+    },
+    {
+        orcamento: 2,
+        paciente: 'juliane Machado',
+        situacao: 'Em espera',
+        celular: '(34) 99246-9033',
+        whatsapp: '(34) 99246-9033',
+        dataorcamento: '07/01/2024',
+        convenio: 'UNIMED'
+    },
+    {
+        orcamento: 3,
+        paciente: 'Elias Aguiar',
+        situacao: 'Em espera',
+        celular: '(34) 99246-9033',
+        whatsapp: '(34) 99246-9033',
+        dataorcamento: '27/11/2023',
+        convenio: 'UNIMED'
+    },
+    {
+        orcamento: 4,
+        paciente: 'Gabriel Silva',
+        situacao: 'Em espera',
+        celular: '(34) 99246-9033',
+        whatsapp: '(34) 99246-9033',
+        dataorcamento: '19/04/2024',
+        convenio: 'UNIMED'
+    }
+]
+var tabela = '<table class="tabela"><tr><th>Orçamento</th><th>Paciente</th><th>Situação</th><th>Celular</th><th>WhatsApp</th><th>Data do Orçamento</th><th>Convênio</th><th>Ações</th></tr>';
+for (let i = 0; i < orcamentos.length; i++) {
+    tabela += `<tr><td>${orcamentos[i].orcamento}</td><td>${orcamentos[i].paciente}</td><td>${orcamentos[i].situacao}</td><td>${orcamentos[i].celular}</td><td>${orcamentos[i].whatsapp}</td><td>${orcamentos[i].dataorcamento}</td><td>${orcamentos[i].convenio}</td><td></td></tr>`;
+}
+tabela += '</table>';
+
+document.querySelector('#conteudo').innerHTML = tabela;
---
+++ sidebar-script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleBtn').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('active');
+  });
+  
